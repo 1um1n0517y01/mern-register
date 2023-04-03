bcfc92dfceeed2fda365f3ca2a49aedf05e51331
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-04-03 02:39:16 +0200

    Implemented User Model Schema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,7 +1,52 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: [true, 'Please tell us your first name.'],
+    trim: true,
+    maxLength: [40, 'A user name can not have more than 40 characters'],
+    minLength: [2, 'A user name can not have less than 2 characters'],
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Please tell us your last name.'],
+    trim: true,
+    maxLength: [40, 'A user name can not have more than 40 characters'],
+    minLength: [2, 'A user name can not have less than 2 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email.'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide email in valid format.'],
+    trim: true,
+    maxLength: [40, 'Email can not have more than 40 characters'],
+    minLength: [8, 'Email name can not have less than 8 characters'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password.'],
+    minLength: [6, 'A user password can not have less than 6 characters'],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    // This only works on save and create
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
+    required: [true, 'Please confirm your password.'],
+    minLength: [6, 'A user password can not have less than 6 characters'],
+  },
+});
 
 const User = mongoose.model('User', userSchema);
 
