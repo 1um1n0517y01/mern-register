662e00ae9f3f4c5e1bedfb79b61cceaf99dedc9e
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-04-03 07:37:53 +0200

    Updates for security

--- server/app.js
+++ server/app.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-app.use(express.json());
+// Set security HTTP headers
+app.use(helmet());
+
+// Develepoment logging
+if (process.env.NODE_ENV === 'development') app.use(morgan('tiny'));
+
+// Limit requests from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour.',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
 app.use(cors());
 
 app.use('/', userRoutes);
